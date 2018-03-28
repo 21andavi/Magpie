8e35bfe6ef6f5c4bdb7ea35fce8f85968c3d9d76
21andavi <37255052+21andavi@users.noreply.github.com>
2018-03-27 20:29:02 -0400

    Update MagPie4.java

--- Project/MagPie4.java
+++ Project/MagPie4.java
@@ -10,7 +10,7 @@
  * @version April 2012
  *
  */
-public class MagPie4
+public class Magpie4
 {
 	/**
 	 * Get a default greeting 	
@@ -49,11 +49,16 @@ public class MagPie4
 		}
 
 		// Responses which require transformations
-		else if (findKeyword(statement, "I want to", 0) >= 0)
+		else if (findKeyword(statement, "I want", 0) >= 0)
 		{
 			response = transformIWantToStatement(statement);
 		}
-
+		 else if (findKeyword(statement, "I", 0) == 0 ) {
+				
+				if(findKeyword(statement, "you", 0) >= 0) {
+					response = transformIYouStatement(statement);
+				}
+		 }		
 		else
 		{
 			// Look for a two word (you <something> me)
@@ -90,9 +95,9 @@ public class MagPie4
 			statement = statement.substring(0, statement
 					.length() - 1);
 		}
-		int psn = findKeyword (statement, "I want to", 0);
-		String restOfStatement = statement.substring(psn + 9).trim();
-		return "What would it mean to " + restOfStatement + "?";
+		int psn = findKeyword (statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 7).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
 	}
 
 	
@@ -103,6 +108,21 @@ public class MagPie4
 	 * @param statement the user statement, assumed to contain "you" followed by "me"
 	 * @return the transformed statement
 	 */
+	private String transformIYouStatement(String statement) {
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+
+		int psnOfI = findKeyword (statement, "I", 0);
+		int psnOfYou = findKeyword (statement, "you", psnOfI + 1);
+
+		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+		return "Why do you " + restOfStatement + " me?";
+	}
 	private String transformYouMeStatement(String statement)
 	{
 		//  Remove the final period, if there is one
